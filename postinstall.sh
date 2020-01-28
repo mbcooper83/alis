0762ff746db1ea3e50a879745fe6a09a3c11674e
mbcooper83 <60344096+mbcooper83@users.noreply.github.com>
2020-01-28 16:13:06 +0000

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-wget -sL https://raw.githubusercontent.com/mbcooper83/alis/master/postinstallbin.sh
+wget https://raw.githubusercontent.com/mbcooper83/alis/master/postinstallbin.sh
 chmod +x postinstallbin.sh
 ./postinstallbin.sh
 rm postinstallbin.sh
