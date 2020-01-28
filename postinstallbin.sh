492f81f202a75abfbec81c8822a0808a172a9988
mbcooper83 <60344096+mbcooper83@users.noreply.github.com>
2020-01-28 12:31:06 +0000

    Update postinstallbin.sh

--- postinstallbin.sh
+++ postinstallbin.sh
@@ -1,5 +1,3 @@
-#! /bin/sh
-
 echo "#############################################"
 echo "      ARCH LINUX - POST INSTALL SCRIPT - MEDIA SERVER CONFIG"
 echo "#############################################"
