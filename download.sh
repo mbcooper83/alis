a02ceda8d98166ac81709032edeb608bd5daede9
mbcooper83 <60344096+mbcooper83@users.noreply.github.com>
2020-02-11 14:55:39 +0000

    Add files via upload

---
+++ download.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+# Arch Media Server Install Script
+wget https://raw.githubusercontent.com/mbcooper83/alis/master/archinstall.sh
+wget https://raw.githubusercontent.com/mbcooper83/alis/master/arch.conf
+chmod +x archinstall.sh
+
