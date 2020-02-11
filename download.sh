3f92ab648ebbce9e14e9a534be42fe889f0a7382
mbcooper83 <60344096+mbcooper83@users.noreply.github.com>
2020-02-11 14:55:12 +0000

    Delete download.sh

--- download.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Arch Media Server Install Script
-rm archinstall.sh
-wget https://raw.githubusercontent.com/mbcooper83/alis/master/archinstall.sh
-chmod +x archinstall.sh
-
